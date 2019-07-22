c2603f11f8cbd57087bfc1215496caae1e77e4f9
Alfonso Vargas <vc.alfonso@hotmail.com>
2019-07-21 22:55:34 -0500

    Configure themeData

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
+        accentColor: Color(0xFF5c6bc0),
+        buttonColor: Color(0xFF5c6bc0),
+        textTheme: TextTheme(
+          headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          title: TextStyle(fontSize: 36.0, fontFamily: 'Poppins', fontWeight: FontWeight.w600),
+          display1: TextStyle(fontSize: 12.0, fontFamily: 'Poppins'),
+        ),
       ),
       home: HomePage(),
     );
